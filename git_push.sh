b29700692d8bbd5067f3305b87e19097e25fa6c9
Werner Victor <wernervic@gmail.com>
2025-02-13 15:37:20 +0200

    Remove Docker PAT from azure_app_settings.json

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,4 @@
 cd /d "c:\Users\werne\my-webapp\NexumAI"
 git add .
-git commit -m "Ensure Docker image names are lowercase"
+git commit -m "Ensure Docker image names are lowercase and correct services"
 git push origin main
