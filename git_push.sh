5119faaab7aa81ad112c7373cb1e543b839446fb
Werner Victor <wernervic@gmail.com>
2025-02-13 15:56:03 +0200

    Update azure_app_settings.json with environment variables

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,6 @@
+### Push Changes to GitHub
+
 cd /d "c:\Users\werne\my-webapp\NexumAI"
 git add .
-git commit -m "Update GitHub Actions workflow to use secrets"
+git commit -m "Update azure_app_settings.json with environment variables"
 git push origin main
